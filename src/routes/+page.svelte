8401aa6703235b03a4fdb4bf35c5c9efa5be048f
olivermallen <allen.ol@northeastern.edu>
2024-02-14 12:07:51 -0500

    step 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import './style.css';
+	import PlayerList from "./PlayerList.svelte";
 
 	// data comes from the load function in +page.js
 	export let data;
@@ -11,10 +12,9 @@
 </script>
 
 <div class="container">
-	<div>{data.dataset[0].name}</div>
-	<div>{xFeature} = {data.dataset[0][xFeature]}</div>
-	<div>{yFeature} = {data.dataset[0][yFeature]}</div>
-	<div>{colorFeature} = {data.dataset[0][colorFeature]}</div>
+	<div class="main">
+		<PlayerList dataset = {data.dataset} />
+	</div>
 </div>
 
 <style>
---
+++ src/routes/PlayerList.svelte
@@ -0,0 +1,19 @@
+<script>
+	// data comes from the load function in +page.js
+	export let dataset;
+</script>
+
+<div class="container">
+</div>
+
+<ul>
+{#each dataset as player}
+<li>{player.name} ({player.team})</li>
+{/each}
+</ul>
+
+<style>
+    li+li {
+        margin-top: 0.5em
+    }
+</style>
